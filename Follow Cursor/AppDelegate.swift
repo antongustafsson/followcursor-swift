d0284208dfe3d4bcbfcc65c828bc57ca9cd5c743
Anton Gustafsson <antonnoren@antons-macbook-pro.local>
2017-05-12 11:52:38 +0200

    Initial Commit

---
+++ Follow Cursor/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Follow Cursor
+//
+//  Created by Anton Gustafsson on 2017-05-12.
+//  Copyright © 2017 Anton Gustafsson. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}

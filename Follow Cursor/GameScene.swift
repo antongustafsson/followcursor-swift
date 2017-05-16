90fa843ceb791705269da8ef7a4a9c20ef126438
Anton Gustafsson <antonnoren@antons-macbook-pro.local>
2017-05-16 13:01:23 +0200

    Initial creation

--- Follow Cursor/GameScene.swift
+++ Follow Cursor/GameScene.swift
@@ -9,57 +9,100 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, GKAgentDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var lastUpdateTime: TimeInterval = 0
+    
+    var playerAgent = GKAgent2D()
+    var targetAgent = GKAgent2D()
+    
+    var playerNode = SKNode()
+    var obstacleNode = SKNode()
+    
+    var seekGoal = GKGoal()
+    var avoidGoal = GKGoal()
+    var speedGoal = GKGoal()
     
     override func didMove(to view: SKView) {
+        print("didMove to view")
+        playerAgent.delegate = self
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let options = [NSTrackingAreaOptions.mouseMoved, NSTrackingAreaOptions.activeInKeyWindow, NSTrackingAreaOptions.activeAlways, NSTrackingAreaOptions.inVisibleRect, ] as NSTrackingAreaOptions
+        let tracker = NSTrackingArea(rect:frame, options: options, owner: view, userInfo: nil)
+        view.addTrackingArea(tracker)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        playerNode = (scene?.childNode(withName: "player"))!
+        obstacleNode = (scene?.childNode(withName: "obstacle"))!
+        var obstacles = SKNode.obstacles(fromNodeBounds: [obstacleNode])
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        print(CGPoint(
+            x: Double(obstacleNode.position.x),
+            y: Double(obstacleNode.position.y)
+        ))
+        
+        seekGoal = GKGoal(toSeekAgent: targetAgent)
+        avoidGoal = GKGoal(toAvoid: obstacles, maxPredictionTime: 10000.0)
+        speedGoal = GKGoal(toReachTargetSpeed: 4.0)
+    }
+    
+    func agentWillUpdate(_ agent: GKAgent) {
+        print(playerAgent.position)
+        print(vector_float2(Float(playerNode.position.x), Float(playerNode.position.y)))
+        playerAgent.position = vector_float2(Float(playerNode.position.x), Float(playerNode.position.y))
+    }
+    
+    func agentDidUpdate(_ agent: GKAgent) {
+        playerNode.physicsBody?.velocity = CGVector(dx: CGFloat(playerAgent.velocity.x), dy: CGFloat(playerAgent.velocity.y))
+//        let distance = distanceBetween(point1: CGPoint(x: CGFloat(targetAgent.position.x), y: CGFloat(targetAgent.position.y)), point2: CGPoint(x: CGFloat(playerAgent.position.x), y: CGFloat(playerAgent.position.y)))
+//        if distance < 200 {
+//            playerAgent.behavior?.setWeight(0.0, for: seekGoal)
+//        }else{
+//            playerAgent.behavior?.setWeight(2.0, for: seekGoal)
+//        }
+//        print("Distance:", distance)
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        print("Touch down, at: \(pos)")
+        
+        
+        playerAgent.radius = Float(playerNode.frame.width)
+        playerAgent.mass = 0.01
+        playerAgent.maxSpeed = 400
+        print(playerNode.frame.width)
+        
+        targetAgent.position.x = Float(pos.x)
+        targetAgent.position.y = Float(pos.y)
+        
+        
+
+        let behaviour = GKBehavior()
+        behaviour.setWeight(1.0, for: speedGoal)
+        behaviour.setWeight(2.0, for: seekGoal)
+        behaviour.setWeight(30.0, for: avoidGoal)
+//        behaviour.setWeight(2.0, for: fleeGoal)
+        
+        playerAgent.behavior = behaviour
+    }
+    
+    func distanceBetween(point1: CGPoint, point2: CGPoint) -> CGFloat{
+        let deltaX = point2.x - point1.x
+        let deltaY = point2.y - point1.y
+        return sqrt(pow(deltaX, 2) + pow(deltaY, 2))
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
+    }
+    
+    func mouseMoved(toPoint pos : CGPoint){
+        targetAgent.position.x = Float(pos.x)
+        targetAgent.position.y = Float(pos.y)
     }
     
     override func mouseDown(with event: NSEvent) {
@@ -70,6 +113,10 @@ class GameScene: SKScene {
         self.touchMoved(toPoint: event.location(in: self))
     }
     
+    override func mouseMoved(with event: NSEvent) {
+        self.mouseMoved(toPoint: event.location(in: self))
+    }
+    
     override func mouseUp(with event: NSEvent) {
         self.touchUp(atPoint: event.location(in: self))
     }
@@ -77,9 +124,8 @@ class GameScene: SKScene {
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
         case 0x31:
-            if let label = self.label {
-                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-            }
+            
+        break
         default:
             print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
         }
@@ -88,5 +134,8 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        let deltaTime = currentTime - lastUpdateTime
+        playerAgent.update(deltaTime: deltaTime)
+        lastUpdateTime = currentTime
     }
 }
